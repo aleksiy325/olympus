8fc987fd7af7652081fe246b103e48bae301eb9e
aleksiy kudelsky <aleksiy kudelsky>
2017-04-09 14:23:19 -0700

    first commit

---
+++ src/frontend/views.py
@@ -0,0 +1,8 @@
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+    	context = super(HomePageView, self).get_context_data(**kwargs)
+    	return context
